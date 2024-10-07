ec3445cc24e1f1bda3c08d6fdcab937365ed217a
Rafael Alexis Pinto Florez <rafael.pintof@opitech.com.co>
2024-10-07 11:43:31 -0500

    Estilos, configuraciones y ruta de archivos

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,12 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	resolve: {
+		alias: {
+		  // Aquí defines los alias
+		  '@': '/src',  // Por ejemplo, @ apunta al directorio src,
+		  '@services': '/src/services'
+		}
+	  }
 });
