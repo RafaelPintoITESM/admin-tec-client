cee977404586315e9f5010cfd3481ce695c96299
Rafael Alexis Pinto Florez <rafael.pintof@opitech.com.co>
2024-10-07 11:43:48 -0500

    Loader y servicio http

---
+++ src/lib/components/Loader.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    import { Spinner } from 'flowbite-svelte';
+    export let isLoading:boolean = true;
+</script>
+
+{#if isLoading}
+<div class="flex justify-center items-center w-full">
+    <Spinner />
+
+</div>
+{:else}
+    <slot />
+{/if}
---
+++ src/services/http/httpClient.ts
@@ -0,0 +1,42 @@
+import {config} from '@/config'
+import { goto } from '$app/navigation';  // Redirección dentro de SvelteKit
+
+export async function get(route:string){
+    let token = localStorage.getItem('token');
+    const response = await fetch(`${config.urlAPI}/${route}`, {
+        method: 'GET',
+        headers: { 
+          'Authorization': 'Bearer ' + token
+        }
+      });
+      if(response.status == 401){
+        localStorage.removeItem('token');
+        goto("/")
+      }
+      console.log(response)
+      return await response.json();
+}
+
+export async function post(route:string ,body:any){
+  let token = localStorage.getItem('token');
+    const response = await fetch(`${config.urlAPI}/${route}`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': 'Bearer ' + token
+        },
+        body: JSON.stringify(body)
+      });
+      return await response.json();
+}
+export async function postForm(route:string ,body:FormData){
+  let token = localStorage.getItem('token');
+    const response = await fetch(`${config.urlAPI}/${route}`, {
+        method: 'POST',
+        headers: {
+          'Authorization': 'Bearer ' + token
+        },
+        body: body
+      });
+      return await response.json();
+}
