687cd069dcd5576b7922b0c1837df79f4569de75
Rafael Alexis Pinto Florez <rafael.pintof@opitech.com.co>
2024-10-11 08:35:52 -0500

    ADD: Eliminacion de programas

--- src/routes/(private)/graduateStudies/+page.svelte
+++ src/routes/(private)/graduateStudies/+page.svelte
@@ -1,18 +1,24 @@
 <script lang="ts">
 	import type { GraduateStudy } from '$lib/models/graduateStudy.model';
-    import { Table, TableBody, TableBodyCell, TableBodyRow, TableHead, TableHeadCell , Button, Popover } from 'flowbite-svelte';
-	import { get } from '@services/http/httpClient';
+    import { Table, TableBody, TableBodyCell, TableBodyRow, TableHead, TableHeadCell , Button, Popover, Modal } from 'flowbite-svelte';
+	import { get ,Delete} from '@services/http/httpClient';
 	import { onMount } from 'svelte';
 	import Loader from '$lib/components/Loader.svelte';
-    import { PlusOutline } from 'flowbite-svelte-icons';
+    import { PlusOutline , ExclamationCircleOutline} from 'flowbite-svelte-icons';
 
     let graduateStudies:GraduateStudy[]=[] ;
     let isLoading = true;
+    let popupModal = false;
     const textLong = 50;
     onMount(async () =>{
+        await getGraduateStudies();
+    })
+
+    const getGraduateStudies = async () =>{
+        isLoading = true;
         graduateStudies = (await get('graduateStudy')) as GraduateStudy[];
         isLoading = false;
-    })
+    }
 
     const isLong = (text:string)=> (text?.length ?? 0) > textLong;
      
@@ -23,6 +29,11 @@
             return text.substring(0, textLong) + '...'
         }
         return text;
+    }
+
+    const deleteProgram = async (id:string)=>{
+        await Delete("graduateStudy/"+id);
+        await getGraduateStudies();
     }
 
 </script>
@@ -51,9 +62,21 @@
                         {/if}
                         <p id="b1-{graduateStudy.id}"> {proccesTextDetail(graduateStudy.detail)}</p>                       
                     </TableBodyCell>
-                    <TableBodyCell><Button href="/graduateStudies/{graduateStudy.id}">Editar</Button></TableBodyCell>
+                    <TableBodyCell>
+                        <Button href="/graduateStudies/{graduateStudy.id}">Editar</Button>
+                        <Button on:click={() => (popupModal = true)} class="ml-2" color="red">Eliminar</Button>
+                        <Modal bind:open={popupModal} size="xs" autoclose>
+                            <div class="text-center">
+                              <ExclamationCircleOutline class="mx-auto mb-4 text-gray-400 w-12 h-12 dark:text-gray-200" />
+                              <h3 class="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">Estas seguro de eliminar el programa de posgrado?</h3>
+                              <Button color="red" class="me-2" on:click={()=> deleteProgram(graduateStudy.id)}>Si, eliminar</Button>
+                              <Button color="alternative">No, cancelar</Button>
+                            </div>
+                          </Modal>
+                    </TableBodyCell>
                 </TableBodyRow>
             {/each}
         </TableBody>
       </Table>
 </Loader>
+
