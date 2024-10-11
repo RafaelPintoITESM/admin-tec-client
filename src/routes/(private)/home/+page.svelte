0f52f83bee94c8fea55d4f08f815830fbfb64ad8
Rafael Alexis Pinto Florez <rafael.pintof@opitech.com.co>
2024-10-11 11:04:04 -0500

    UPDATE: Link en home

--- src/routes/(private)/home/+page.svelte
+++ src/routes/(private)/home/+page.svelte
@@ -1,1 +1,8 @@
+<script>
+
+	import { Button } from "flowbite-svelte";
+
+</script>
 <h1>Bienvenido al portal administrativo del TEC</h1>
+
+<Button href="graduateStudies">Programas de Posgrado</Button>
